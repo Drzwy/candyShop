3c96fafc299bed2ab323244afdeb8e973f9cc4d4
Drzwy <tomassilva0360@gmail.com>
2023-12-20 14:25:25 -0300

    se arreglo echando a perder otra cosa
    subject? as observable? no puedo definirlas antes de suscribirme

--- src/app/components/admin/viewCandy/viewCandy.component.ts
+++ src/app/components/admin/viewCandy/viewCandy.component.ts
@@ -17,14 +17,16 @@ export class ViewCandyComponent implements OnInit {
 
   ngOnInit() {
     this.subscription.add(
-      this.adminService.seePreview().subscribe((candyPreview) => {
-        this.candyPreview = candyPreview;
+      this.adminService.seePreview().subscribe((candy) => {
+        this.candyPreview = candy;
+        console.log(candy);
       })
     );
 
     this.subscription.add(
       this.adminService.hasChanged().subscribe((hasChanged) => {
         this.hasBeenChanged = hasChanged;
+        console.log(hasChanged);
       })
     );
 
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { CandyStorage } from '../models/CandyStorage';
-import { Observable, of } from 'rxjs';
+import { Observable, Subject, of, scheduled, shareReplay } from 'rxjs';
 import { Candy } from '../models/Candy';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminService {
-  public hasBeenChanged = false;
+  private hasBeenChanged = new Subject<boolean>();
+  public hasBeenChanged$: Observable<boolean> =
+    this.hasBeenChanged.asObservable();
 
-  public candyStoragePreview!: CandyStorage;
+  private candyStoragePreview = new Subject<CandyStorage>();
+  public candyStoragePreview$: Observable<CandyStorage> =
+    this.candyStoragePreview.asObservable();
 
   constructor() {
-    this.candyStoragePreview = new CandyStorage(
+    let previewCandy = new CandyStorage(
       new Candy(
         'Test',
         'Description',
@@ -23,25 +27,28 @@ export class AdminService {
       ),
       0
     );
+    this.candyStoragePreview.next(previewCandy);
   }
 
   public pushPreview(candyStorage: CandyStorage): Observable<boolean> {
     try {
-      this.candyStoragePreview = candyStorage;
-      this.hasBeenChanged = true;
-    } catch {
-      return of(false);
-    } finally {
+      this.candyStoragePreview.next(candyStorage);
+      this.hasBeenChanged.next(true);
       console.log(this.candyStoragePreview);
       return of(true);
+    } catch (error) {
+      console.error(error);
+      return of(false);
     }
   }
 
   public seePreview(): Observable<CandyStorage> {
-    return of(this.candyStoragePreview);
+    // console.log(this.candyStoragePreview);
+    return this.candyStoragePreview$;
   }
 
   public hasChanged(): Observable<boolean> {
-    return of(this.hasBeenChanged);
+    // console.log(this.hasBeenChanged);
+    return this.hasBeenChanged$;
   }
 }
